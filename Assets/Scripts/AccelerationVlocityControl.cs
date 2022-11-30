74705c06bd20fbf72cd1f1349de2c2512045005f
Lylilaaaa <2486132028@qq.com>
2022-11-30 21:06:20 +0800

    dynamic control

--- Assets/Scripts/AccelerationVlocityControl.cs
+++ Assets/Scripts/AccelerationVlocityControl.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace DefaultNamespace
 {
@@ -8,6 +9,8 @@ namespace DefaultNamespace
         public Vector3 accInput = Vector3.zero;
         public Vector3 angInput = Vector3.zero;
         public float acc_g = 9.80665f;
+        public string sensorInput;
+
         //private Vector3 lastInput;
         public Vector3 velocityShown;
         public Vector3 rotationShown;
@@ -15,6 +18,9 @@ namespace DefaultNamespace
         private Rigidbody rigi;
         private Vector3 lastVelocity;
         private Vector3 nowVelocity;
+        private Vector3 lastAngleInput;
+
+        private float[] senSorFloats;
         //private float timer;
 
         private void Start()
@@ -24,21 +30,33 @@ namespace DefaultNamespace
             lastVelocity = rigi.velocity;
             //timer = 0f;
             velocityShown = rigi.velocity;
+            lastAngleInput = Vector3.zero;
         }
 
-        private void FixedUpdate()//0.02s
+        public void Ticked()//0.02s
         {
-            //速度控制
+            //结果展示
+            //print("sensorInput: "+sensorInput);
             velocityShown = rigi.velocity;
             rotationShown = rigi.rotation.eulerAngles;
+            
+            float[] senSorFloats = new float[6];
+            senSorFloats = StringSplit(sensorInput);
+            accInput = new Vector3(senSorFloats[0], senSorFloats[1], senSorFloats[2]);
+            angInput = new Vector3(senSorFloats[3], senSorFloats[4], senSorFloats[5]);
+            
             //lastInput = accInput;
             nowVelocity = lastVelocity + accInput * acc_g * 0.02f;//v = v0 + at
             lastVelocity = nowVelocity;
             rigi.velocity = nowVelocity;
             
             //角度控制
-            rigi.rotation = Quaternion.Euler(angInput);
-            
+            SetX(lastAngleInput,angInput);
+            SetY(lastAngleInput,angInput);
+            SetZ(lastAngleInput,angInput);
+            lastAngleInput = angInput;
+            //rigi.rotation = Quaternion.Euler(angInput);
+
             // timer += Time.fixedDeltaTime;//当加速度没有产生变化时，计时器启动
             // if ((accInput - lastInput).magnitude > 0.01)//input的加速器发生变化（能够微调的传感器几乎一直在变化）
             // {
@@ -49,7 +67,50 @@ namespace DefaultNamespace
             //     rigi.velocity = nowVelocity;
             //     timer = 0f;//一个速度变化发生，计时器清零
             // }
+
+        }
+
+        private float[] StringSplit(string inputString)
+        {
+            var data1 = inputString.Split(' ');
+            List<string> splitedList = new List<string>();
+            foreach (string s in data1)
+            {
+                if (s.Trim() != "")
+                {
+                    splitedList.Add(s);
+                }
+            }
             
+            float[] outputString = new float[6];
+            
+            outputString[0] = float.Parse(splitedList[1]);
+            outputString[1] = float.Parse(splitedList[2]);
+            outputString[2] = float.Parse(splitedList[3]);
+            outputString[3] = float.Parse(splitedList[9]);
+            outputString[4] = float.Parse(splitedList[10]);
+            outputString[5] = float.Parse(splitedList[11]);
+            //senSorFloats = outputString;
+            print("outputstring ="+outputString[0]+outputString[1]+outputString[2]+outputString[3]+outputString[4]+outputString[5]);
+            return outputString;
+        }
+        void SetX(Vector3 oldAngleInput,Vector3 nowAngleInput)
+        {
+            Quaternion t_adj = Quaternion.AngleAxis(nowAngleInput.x-oldAngleInput.x, Vector3.up);
+            Quaternion t_delta = gameObject.transform.localRotation * t_adj;
+            gameObject.transform.localRotation = t_delta;
+        }
+        void SetY(Vector3 oldAngleInput,Vector3 nowAngleInput)
+        {
+            Quaternion t_adj = Quaternion.AngleAxis(nowAngleInput.y-oldAngleInput.y, -Vector3.right);
+            Quaternion t_delta = gameObject.transform.localRotation * t_adj;
+            gameObject.transform.localRotation = t_delta;
+        }
+        void SetZ(Vector3 oldAngleInput,Vector3 nowAngleInput)
+        {
+            Quaternion t_adj = Quaternion.AngleAxis(nowAngleInput.z-oldAngleInput.z, Vector3.forward);
+            Quaternion t_delta = gameObject.transform.localRotation * t_adj;
+            gameObject.transform.localRotation = t_delta;
         }
     }
 }
--- Assets/Scripts/ReadData.cs
+++ Assets/Scripts/ReadData.cs
@@ -14,7 +14,7 @@ namespace DefaultNamespace
         private void Start()
         {
             //dataset = File.ReadAllText(Application.streamingAssetsPath + "/dataWrite.txt");
-            TryReadData(Application.streamingAssetsPath + "/dataWrite.txt");
+            TryReadData(Application.streamingAssetsPath + "/dataWrite.txt",Application.streamingAssetsPath + "/dataWriteAng.txt");
             // string testLine = "today[you are OK!";
             // var linePart = testLine.Split('[');
             // print(linePart[0]);
@@ -34,10 +34,10 @@ namespace DefaultNamespace
         //     TryReadData(Application.streamingAssetsPath + "/dataWrite.txt",timeTrigger);
         // }
 
-        public void TryReadData(string path)
+        public void TryReadData(string path1,string path2)
         {
-            var accReader = new StreamReader(path);
-            var aguReader = new StreamReader(path);
+            var accReader = new StreamReader(path1);
+            var aguReader = new StreamReader(path1);
             StartCoroutine(WaitAndRead(accReader,aguReader));
             // while ((line = sr.ReadLine()) != null)
                 // {
@@ -62,8 +62,8 @@ namespace DefaultNamespace
                 var data1 = line1.Split('[');
                 var data2 = line2.Split('[');
                 
-                print("time: "+Time.time);
-                print( "data:"+data1[0]+' '+data1[1]+' '+data1[2]);
+                //print("time: "+Time.time);
+                //print( "data:"+data1[0]+' '+data1[1]+' '+data1[2]);
                 acVcon.accInput = new Vector3(float.Parse(data1[0]),float.Parse(data1[1]),float.Parse(data1[2]));
                 acVcon.angInput = new Vector3(float.Parse(data2[0]),float.Parse(data2[1]),float.Parse(data2[2]));
             }
